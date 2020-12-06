b2ec6154074ba7c21c092decf05703cfc7291e8a
ArciducaZagaria <pierluigizagaria@gmail.com>
2020-12-06 12:47:18 +0100

    Major rewrite with updated packages.

---
+++ app.js
@@ -0,0 +1,149 @@
+import request from 'request';
+import { join } from 'path';
+import { statSync } from 'fs';
+import { spawn } from 'child-process-promise';
+import { parse } from '@node-steam/vdf';
+
+var _ = {}
+_.defaults = require('lodash.defaults')
+
+var defaultOptions = {
+	binDir: join(__dirname, 'steamcmd_bin')
+}
+
+var downloadSteamCMD = function (opts) {
+	opts = _.defaults(opts, defaultOptions);
+	var url, extractor;
+	if (process.platform === 'win32') {
+		url = 'https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip'
+		extractor = require('unzipper')
+	} else if (process.platform === 'darwin') {
+		url = 'https://steamcdn-a.akamaihd.net/client/installer/steamcmd_osx.tar.gz'
+		extractor = require('tar')
+	} else if (process.platform === 'linux') {
+		url = 'https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz'
+		extractor = require('tar')
+	} else {
+		return _reject('Unsupported platform')
+	}
+	return new Promise(function (resolve, reject) {
+		var req = request(url)
+		if (process.platform !== 'win32') {
+			req = req.pipe(require('zlib').createGunzip())
+		}
+		req.pipe(extractor.Extract({path: opts.binDir})
+			.on('finish', resolve)
+			.on('error', reject)
+		)
+	})
+}
+
+var download = function (opts) {
+	opts = _.defaults(opts, defaultOptions)
+	try {
+		statSync(opts.binDir)
+		return _resolve()
+	} catch (err) {
+		return download(opts)
+	}
+}
+
+var run = function (commands, opts) {
+	opts = _.defaults(opts, defaultOptions)
+	var exeName
+	if (process.platform === 'win32') {
+		exeName = 'steamcmd.exe'
+	} else if (process.platform === 'darwin') {
+		exeName = 'steamcmd.sh'
+	} else if (process.platform === 'linux') {
+		exeName = 'steamcmd.sh'
+	} else {
+		return _reject('Unsupported platform')
+	}
+	var args = commands.concat('quit').map(function (x) {
+		return '+' + x
+	}).join(' ').split(' ')
+	return new Promise(function (resolve, reject) {
+		spawn(join(opts.binDir, exeName), args, {
+			capture: ['stdout', 'stderr'],
+			cwd: opts.binDir
+		})
+		.then((x) => { 
+			resolve(x); 
+		})
+		.catch((x) => {
+			// For some reason, steamcmd will occasionally exit with code 7 and be fine.
+			// This usually happens the first time touch() is called after download().
+			(x.code === 7) ? resolve(x) : reject(x)
+		})
+	})
+}
+
+var touch = function (opts) {
+	opts = _.defaults(opts, defaultOptions)
+	return run([], opts)
+}
+
+var getAppInfo = function (appID, opts) {
+	opts = _.defaults(opts, defaultOptions)
+
+	// The first call to app_info_print from a new install will return nothing,
+	// and it will instead prep an entry for the info and request it.
+	// It won't block though, and if the session closes before it can save,
+	// the same issue will be present on next run.
+	// Thus we use `app_update` to force the session to wait long enough to save.
+	var forceInfoCommand = [
+		'@ShutdownOnFailedCommand 0', 'login anonymous',
+		'app_info_print ' + appID,
+		'force_install_dir ./4', 'app_update 4'
+	]
+
+	// The output from app_update can collide with that of app_info_print,
+	// so after ensuring the info is available we must re-run without app_update.
+	var fetchInfoCommand = [
+		'@ShutdownOnFailedCommand 0', 'login anonymous',
+		'app_info_update 1', // force data update
+		'app_info_print ' + appID,
+		'find e' // fill the buffer so info's not truncated on Windows
+	]
+
+	return new Promise((resolve, reject) => {
+		run(forceInfoCommand, opts) // @todo only force when needed
+			.then(() => {
+				return run(fetchInfoCommand, opts)
+			})
+			.then((proc) => {
+				var stdout = proc.stdout.replace('\r\n', '\n')
+				var infoTextStart = stdout.indexOf('"' + appID + '"')
+				var infoTextEnd = stdout.indexOf('ConVars:')
+				var infoText = stdout.substr(infoTextStart, infoTextEnd - infoTextStart)
+				return resolve(parse(infoText)[appID]);
+			})
+			.catch((e) => reject(e));
+	});
+}
+
+var prep = function (opts) {
+	opts = _.defaults(opts, defaultOptions)
+	return new Promise((resolve, reject) => {
+		downloadSteamCMD(opts)
+			.then(() => { 
+				return new Promise(resolve => setTimeout(resolve, 500))})
+			.then(() => { 
+				return touch(opts); 
+			})
+			.then(() => { 
+				return resolve(); 
+			})
+			.catch((e) => { 
+				reject(e); 
+			});
+	});
+}
+
+export default {
+	download,
+	touch,
+	prep,
+	getAppInfo
+}
